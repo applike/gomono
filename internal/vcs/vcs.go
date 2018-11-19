fb56392ff1e12d6ad4b409555c223f20ec3591e1
Mathis Engelbart <mathis@applike.info>
2018-11-19 17:34:33 +0100

    cleanup vcs

--- internal/vcs/vcs.go
+++ internal/vcs/vcs.go
@@ -4,18 +4,12 @@ import (
 	"fmt"
 	"os"
 	"os/exec"
+	"path/filepath"
 	"strings"
 )
 
-// Changed returns whether a package changed between the first and the second commit
-func Changed(pkg, first, second string) bool {
-	if strings.Contains(pkg, "/vendor/") {
-		return false
-	}
-	return changed(pkg, first, second)
-}
-
-func changed(dir, first, second string) bool {
+// Changed returns whether a directory changed between the first and the second commit
+func Changed(dir, first, second string) bool {
 	var (
 		cmdOut []byte
 		err    error
@@ -36,11 +30,52 @@ func Show(path, rev string) (string, error) {
 		cmdOut []byte
 		err    error
 	)
-	cmd := exec.Command("git", "show", fmt.Sprintf("%v:%v", rev, path))
+
+	rel, err := absFromRoot(path)
+	if err != nil {
+		return "", err
+	}
+	root, err := root()
+	if err != nil {
+		return "", err
+	}
+	git := filepath.Join(root, ".git")
+	cmd := exec.Command(
+		"git",
+		fmt.Sprintf("%s%s", "--work-tree=", root),
+		fmt.Sprintf("%s%s", "--git-dir=", git),
+		"show",
+		fmt.Sprintf("%s:%s", rev, rel),
+	)
 	cmd.Stderr = os.Stderr
 	if cmdOut, err = cmd.Output(); err != nil {
 		return "", err
 	}
 
 	return string(cmdOut), nil
+}
+
+// AbsFromRoot returns the path of a file in a git repository
+// with the root path of that git directory removed
+func absFromRoot(p string) (string, error) {
+	root, err := root()
+	if err != nil {
+		return "", err
+	}
+
+	return p[len(root)+1:], nil
+}
+
+func root() (string, error) {
+	var (
+		cmdOut []byte
+		err    error
+	)
+	cmd := exec.Command("git", "rev-parse", "--show-toplevel")
+	cmd.Stderr = os.Stderr
+	if cmdOut, err = cmd.Output(); err != nil {
+		return "", err
+	}
+
+	return strings.TrimSpace(string(cmdOut)), nil
 }
